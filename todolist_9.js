fe72eb0d8d086db6e85638d2b01730c84d2e00ab
Funmi <folowookere@yahoo.com>
2018-05-09 15:57:58 +0000

    Lesson 9

--- todolist_9.js
+++ todolist_9.js
@@ -2,43 +2,25 @@
 
 var todoList = {
   todos: [],
-  displayToDos: function() {
-  if (this.todos.length === 0) {
-    console.log("Your to do list is empty");
-  } else {
-   console.log('My todos:');
-   for (var i = 0; i < this.todos.length; i++) {
-    if (this.todos[i].completed === true) {
-          console.log('(x)', this.todos[i].todoText);
-      } else {
-            console.log('( )', this.todos[i].todoText);
-      }
-    }
-   }
-  },
   addToDo: function(todoText) {
     
     this.todos.push({
       todoText: todoText,
       completed: false
     });
-    this.displayToDos();
   },
   changeToDo: function(position, todoText) {
  
    this.todos[position].todoText = todoText; 
-    this.displayToDos();
   },
   deleteToDo: function(position) {
      
     this.todos.splice(position, 1);
-    this.displayToDos();
   },
   toggleCompleted: function(position){
     
     var todos = this.todos[position];
     todos.completed = !todos.completed;
-    this.displayToDos();
   },
   toggleAll: function() {
     var totalToDos = this.todos.length;
@@ -62,14 +44,10 @@ var todoList = {
           this.todos[i].completed = true;
         }
       }
-    this.displayToDos();
   }
 };
 
 var handlers = {
-  displayToDos: function() {
-    todoList.displayToDos();
-  },
   addToDo: function() {
     var addToDoTextInput = document.getElementById('addToDoTextInput');
     todoList.addToDo(addToDoTextInput.value);
